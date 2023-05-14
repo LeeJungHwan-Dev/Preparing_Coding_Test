1c38a5ee3508e2b601b132dd66995243db9c4934
LeeJungHwan-Dev <aftermoa@gmail.com>
2023-05-14 21:06:03 +0900

    Add : baekJoon-1764 solved

---
+++ BaekJoon_Solved/baekJoon-1764/src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    var size = readln().split(" ").map { it.toInt() }
+    var notSee = HashSet<String>()
+    var list = ArrayList<String>()
+    var str = StringBuilder()
+    var count = 0
+
+    repeat(size[0]){
+        notSee.add(readln())
+    }
+
+    repeat(size[1]){
+        var item = readln()
+        if (notSee.contains(item)){
+            count++
+            list.add(item)
+        }
+    }
+    list.sort()
+
+    list.forEach {
+        str.append("$it\n")
+    }
+
+    println(count)
+    println(str)
+}
