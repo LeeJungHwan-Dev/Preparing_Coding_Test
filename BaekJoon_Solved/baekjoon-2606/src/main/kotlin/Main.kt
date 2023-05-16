a68e343193f7ec450bc2c283833b849560cb2885
LeeJungHwan-Dev <aftermoa@gmail.com>
2023-05-16 22:48:09 +0900

    Update : baekJoon-2606 code optimization

--- BaekJoon_Solved/baekjoon-2606/src/main/kotlin/Main.kt
+++ BaekJoon_Solved/baekjoon-2606/src/main/kotlin/Main.kt
@@ -1,30 +1,32 @@
+lateinit var map : Array<MutableList<Int>>
+lateinit var visited : Array<Boolean>
 var answer = 0
+fun main(args: Array<String>) {
+    var maxComputer = readln().toInt()
+    var lineInput = readln().toInt()
 
-fun main(args: Array<String>){
-    var size = readln().toInt()
-    var count = readln().toInt()
-    var map = Array<MutableList<Int>>(size + 1){ mutableListOf() }
-    var isCheck = Array<Boolean>(size + 1) {false}
+    map = Array(maxComputer+1){ mutableListOf() }
+    visited = Array(maxComputer+1){false}
 
-    repeat(count){
-        var line = readln().split(" ").map { it.toInt() }
+    repeat(lineInput){
+        var inputs = readln().split(" ").map { it.toInt() }
 
-        map[line[0]].add(line[1])
-        map[line[1]].add(line[0])
+        map[inputs[0]].add(inputs[1])
+        map[inputs[1]].add(inputs[0])
     }
 
-    dfs(map,isCheck,1)
-    println(answer)
+    dfs(1)
 
+    println(answer)
 }
 
-fun dfs(map: Array<MutableList<Int>>, isCheck: Array<Boolean>, startIndex:Int){
-    isCheck[startIndex]= true
+fun dfs(startNode: Int){
+    visited[startNode] = true
 
-    for (i2 in map[startIndex]){ //핵심은 startIndex 즉 1번과 연결된 노드들만 순회할 수 있게 인접 리스트에 담긴 항목을 이용해 접근해서 4번과 7번은 해당되지 않음
-        if (!isCheck[i2]){
-            answer ++
-            dfs(map,isCheck,i2)
+    for (i in map[startNode]){
+        if (!visited[i]){
+            answer++
+            dfs(i)
         }
     }
 }
